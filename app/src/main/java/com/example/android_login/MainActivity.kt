b149a67860825ef63ebd79a7861e5b4c9b072c4f
Halil Akgun <halilakgun0@gmail.com>
2024-12-21 09:32:34 +0100

    full-screen

--- app/src/main/java/com/example/android_login/MainActivity.kt
+++ app/src/main/java/com/example/android_login/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // This code enables the app to operate in full-screen mode by handling system bar insets
+        // and applying appropriate padding to the content view.
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
